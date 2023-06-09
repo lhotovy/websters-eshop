6187dcc20c4f9bda346a6d2dc90a662295915db4
lhotovy <lhotovy@centrum.cz>
2023-06-09 07:31:08 +0200

    Added footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import { Footer } from '@/components/footer'
 import { Header } from '@/components/header'
-import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Header/>      
+      <Header/>
+      <Footer />      
     </main>
   )
 }
