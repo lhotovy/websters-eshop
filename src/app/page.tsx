f5c53bed4ae2fd19f4d1eb0b33327295a2d74e2f
lhotovy <lhotovy@centrum.cz>
2023-06-09 11:54:09 +0200

    Setup favicon and hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Footer } from '@/components/footer'
 import { Header } from '@/components/header'
+import { Hero } from '@/components/hero'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Header/>
+      <Hero />
       <Footer />      
     </main>
   )
