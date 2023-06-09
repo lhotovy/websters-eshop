a237dbe4b7fa8c45a02465eedeb085ba9ffb4044
lhotovy <lhotovy@centrum.cz>
2023-06-09 07:31:31 +0200

    App adjustments

---
+++ src/data/navLinks.ts
@@ -0,0 +1,14 @@
+const navLinks = [
+    {
+        name: "Home",
+        href: "/home"
+    },
+    {
+        name: "E-shop",
+        href: "/shop"
+    },
+    {
+        name: "Contact Us",
+        href: "/contact"
+    }
+]
