9ae5262af0ebbb26eb5fb7a5e2285d907b77015c
lhotovy <lhotovy@centrum.cz>
2023-06-12 13:19:23 +0200

    Fix lint

--- src/data/navLinks.ts
+++ src/data/navLinks.ts
@@ -1,14 +1,17 @@
 export const navLinks = [
     {
         name: "Home",
+        id: 1,
         href: "/"
     },
     {
         name: "E-shop",
+        id: 2,
         href: "/eshop"
     },
     {
         name: "Contact Us",
+        id: 3,
         href: "/contact"
     }
 ]
