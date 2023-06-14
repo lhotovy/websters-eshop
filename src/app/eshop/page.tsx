514dbf81e53c7f25491ffa4db80fc9e0b8d6529b
lhotovy <lhotovy@centrum.cz>
2023-06-14 08:20:51 +0200

    Added types and eshop page content

--- src/app/eshop/page.tsx
+++ src/app/eshop/page.tsx
@@ -1,12 +1,13 @@
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
-import { ProductList } from "@/components/productList";
+import  ProductList from "@/components/productList";
+import { products } from "@/data/products";
 
 export default function Page() {
   return (
     <>
       <Header/>
-      <ProductList/>
+      <ProductList products={products}/>
       <Footer/>
     </>
   )
--- src/components/productList.tsx
+++ src/components/productList.tsx
@@ -1,5 +1,31 @@
-export const ProductList = () => {
-  return (
-    <p className="text-white flex justify-center items-center mt-64">Here come our products</p>
-  )
+import Link from "next/link";
+import React from 'react';
+
+export type Product = {
+  title: string;
+  name: string;
+  image: string;
+  info: string;
 }
+interface ProductListProps {
+  products: Product[]
+};
+
+const ProductList: React.FC<ProductListProps> = ({products}) => {
+  return (
+    <div className="products m-auto mt-48  w-2/3 h-1/3 grid grid-cols-3 gap-6 justify-center">
+      {products.map((product: any)=> {
+        return (      
+          <div className="product-container col-span-1 justify-center border-solid border-sky-500 border-2 rounded-xl p-6">
+            <Link href={`/eshop/${product.title}`}>
+                <img src={product.image} alt={product.name} className="w-64 m-auto text-center drop-shadow-xl rounded-xl" />
+                <h5 className="text-center text-lg uppercase m-2">{product.name}</h5>
+                <p className="text-center">{`${product.price} €`}</p>
+            </Link>
+          </div>          
+      )})}
+    </div>
+  );
+};
+
+export default ProductList;
