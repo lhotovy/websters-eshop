dd781a73f216aae443212bdec0acf9f03807dc70
lhotovy <lhotovy@centrum.cz>
2023-06-14 11:36:09 +0200

    Added tags

--- src/app/eshop/page.tsx
+++ src/app/eshop/page.tsx
@@ -2,11 +2,13 @@ import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
 import  ProductList from "@/components/productList";
 import { products } from "@/data/products";
+import Tags from "@/components/tags";
 
 export default function Page() {
   return (
     <>
       <Header/>
+      <Tags/>
       <ProductList products={products}/>
       <Footer/>
     </>
---
+++ src/components/tags.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Tags: React.FC = () => {
+    return (
+        <section className='block w-4/5 text-right'>            
+            <button type="button" className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">Home</button>
+            <button type="button" className="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-3 py-2 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">Art</button>
+            <button type="button" className="text-purple-700 hover:text-white border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-3 py-2 text-center mr-2 mb-2 dark:border-purple-400 dark:text-purple-400 dark:hover:text-white dark:hover:bg-purple-500 dark:focus:ring-purple-900">Free time</button>
+        </section>
+    );
+};
+
+export default Tags;
