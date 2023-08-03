ab263851c98a7ad5b9b9ca44b787a3386835fc19
lhotovy <lhotovy@centrum.cz>
2023-08-03 12:12:55 +0200

    Separated types

---
+++ src/lib/types.ts
@@ -0,0 +1,58 @@
+import { BaseSyntheticEvent } from 'react';
+
+export interface IFormSubmission {
+    isError?: boolean
+    isLoading?: boolean
+};
+
+export type FormData = {
+    firstName: string
+    lastName: string
+    email: string
+    textInput: string
+};
+
+export interface ProductParams {
+    product: string
+};
+
+export interface IContactForm {
+    handleSubmit?: (event: BaseSyntheticEvent) => {} 
+    isSuccess?: boolean
+    formData: FormData
+    setData: (formData: FormData) => void
+};
+
+export type Product = {
+    id: number
+    title: string
+    name: string
+    image: string
+    info: string
+    price: number
+    tags: string
+};
+
+export interface ProductListProps {
+    categories: string[]
+    shopItems: Product[]
+    filterItems: (category: string) => void
+};
+
+export interface ITagsProps {
+    categories: string[]
+    filterItems: (category: string) => void
+};
+
+export type Cart = {
+    [key: number]: number | undefined
+};
+  
+export type Context = {
+      cartItems: Cart
+      addToCart: (itemId: number) => void
+      removeFromCart: (itemId: number) => void
+      updateCartItemCount: (newAmount: number, itemId: number) => void
+      getCartTotal: () => number
+      getNumberOfItems: () => number
+};
