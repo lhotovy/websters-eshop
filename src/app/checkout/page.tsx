f117da31a1bcaba1583311f4d9fe905c18154c50
lhotovy <lhotovy@centrum.cz>
2023-08-15 13:14:28 +0200

    Minor fixes

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -6,7 +6,7 @@ const Checkout = () => {
     <div className="flex justify-center flex-col min-h-screen items-center">
       <Header />
       <div className="flex items-center justify-center md:w-full w-2/3 text-justify md:text-xl text-lg">
-        Sorry, we do not have checkout process deployed yet. For now, just enjoy experience from out E-shop UI.
+        Sorry, we do not have checkout process deployed yet. For now, just enjoy experience from our E-shop UI.
       </div>
       <EshopButton />
     </div>
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import { Header } from "@/components/header";
 import { useSession } from "next-auth/react";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import Link from "next/link";
+import EshopButton from "@/components/eshopButton";
 
 const Dashboard = () => {
 
@@ -24,14 +24,9 @@ const Dashboard = () => {
                     Hi, {session?.user?.name}
                 </div>
                 <p className="md:w-4/5 md:text-center w-2/3 text-justify">
-                    This is you personal page. You'll be able to do more here in the future. For now just enjoy our 
-                    <span className="ml-3 text-blue-300">  
-                        <Link href="/eshop">
-                            e-shop
-                        </Link>
-                    </span>                  
-                !
-                </p>              
+                    This is your personal page. You'll be able to do more here in the future. For now just enjoy our e-shop!              
+                </p>
+                <EshopButton />              
             </div>            
         </>    
     );
