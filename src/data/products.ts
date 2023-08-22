baa0b830a42f4e51275ff92ee80403c346eb75ea
lhotovy <lhotovy@centrum.cz>
2023-08-22 12:15:39 +0200

    Styling eshop pictures

--- src/data/products.ts
+++ src/data/products.ts
@@ -5,7 +5,7 @@ export const products: Product[] = [
         id: 1,
         title: "cup",
         name: "Wooden cuppery",
-        image: "/cup.jpg",
+        image: "/cup1.jpg",
         info: "Our Wooden Cups are meticulously handcrafted by skilled artisans who possess a deep reverence for woodworking. Using only the finest quality wood, they skillfully shape and carve each cup, ensuring exceptional craftsmanship and attention to detail. The result is a stunning masterpiece that showcases the natural beauty of the wood while providing a durable and functional vessel for your beverages.",
         price: 10,
         tags: "Home"
@@ -14,7 +14,7 @@ export const products: Product[] = [
         id: 2,
         title: "carvings",
         name: "3D Wood carvings",
-        image: "/carving.jpg",
+        image: "/carving1.jpg",
         info: "Introducing our collection of exquisite Wooden Carvings, where the art of carving merges with the natural allure of wood to create captivating pieces that inspire awe and admiration. Each carving is meticulously crafted by skilled artisans, showcasing the intricacy of their craft and the inherent beauty of the wood.",
         price: 50,
         tags: "Art"
@@ -32,7 +32,7 @@ export const products: Product[] = [
         id: 4,
         title: "chess",
         name: "Wooden chess",
-        image: "/chess.jpg",
+        image: "/chess1.jpg",
         info: "Introducing our exquisite Wooden Chess Set, a timeless classic that combines the artistry of craftsmanship with the strategic depth of the game of kings. Meticulously handcrafted using premium wood, this chess set is designed to bring endless hours of intellectual challenge and aesthetic pleasure to chess enthusiasts of all levels.",
         price: 100,
         tags: "Free time"
@@ -41,7 +41,7 @@ export const products: Product[] = [
         id: 5,
         title: "candleHolder",
         name: "Candle holder",
-        image: "/candleHolder.jpg",
+        image: "/candleHolder1.jpg",
         info: "Introducing our collection of beautifully crafted Wooden Candle Holders, designed to enhance the soothing glow of candlelight and add a touch of natural elegance to your space. Each candle holder is meticulously handcrafted from premium wood, showcasing the perfect blend of artistry and functionality.",
         price: 10,
         tags: "Home"
